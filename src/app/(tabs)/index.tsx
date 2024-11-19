756aa86092528dc6ac9a2c3db79e6abd60b96efd
Muhammad Irfan Khan <sahir9484@gmail.com>
2024-11-19 15:41:22 +0500

    types checking in .contd

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,13 +4,16 @@ import EditScreenInfo from '@/src/components/EditScreenInfo';
 import products from '../../../assets/data/products';
 
 import ProductListItem from '../../components/ProductListItem';
+  
 
 
+
+ 
 export default function MenuScreen() {
   return (
     <View>
 
-      <ProductListItem product= {products[5]}/>
+      <ProductListItem product= {products[0]}/>
 
     </View>
   );
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 
 import Colors from "../constants/Colors";
+import {Product} from '../types';
 
-const ProductListItem = ({  product }) => {
+type ProductListItemProps ={
+    product:Product;
+}
+
+export   const DefaultPizzaImage= 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+const ProductListItem = ({  product }:ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image source={{ uri: product.image ||  DefaultPizzaImage}} style={styles.image} />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
